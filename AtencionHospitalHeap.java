f64b19c417ca2a39bcfb2d6b9b1482642669c47e
Deniss3232 <iqdenis@gmail.com>
2025-04-06 22:56:22 -0600

    Update AtencionHospitalHeap.java

--- AtencionHospitalHeap.java
+++ AtencionHospitalHeap.java
@@ -1,1 +1,36 @@
+import java.io.File;
+import java.util.Scanner;
 
+// Este programa usa tu propia cola con prioridad para atender pacientes
+public class AtencionHospitalHeap {
+    public static void main(String[] args) {
+        ColaPrioridadHeap colaPacientes = new ColaPrioridadHeap();
+
+        try {
+            File archivo = new File("pacientes.txt");
+            Scanner lector = new Scanner(archivo);
+
+            while (lector.hasNextLine()) {
+                String linea = lector.nextLine();
+                String[] datos = linea.split(",");
+
+                String nombre = datos[0].trim();
+                String sintomas = datos[1].trim();
+                char prioridad = datos[2].trim().charAt(0);
+
+                FichaPaciente nuevo = new FichaPaciente(nombre, sintomas, prioridad);
+                colaPacientes.agregar(nuevo);
+            }
+
+            lector.close();
+
+            System.out.println("Pacientes en el orden que deben ser atendidos:");
+            while (!colaPacientes.estaVacia()) {
+                System.out.println(colaPacientes.siguiente());
+            }
+
+        } catch (Exception e) {
+            System.out.println("Ocurrió un error al leer el archivo: " + e.getMessage());
+        }
+    }
+}
